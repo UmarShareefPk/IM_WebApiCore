c32eade3e3afe0bf0b1067229f61e02383e68364
Umar Shareef <Omar.Sharif91@gmail.com>
2021-01-12 02:10:12 +0500

    Update UsersController.cs

--- ApiControllers/UsersController.cs
+++ ApiControllers/UsersController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using IM.Common;
 using IM.Models;
 using IM.SQL;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -15,12 +17,16 @@ namespace IM_Core.ApiControllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        public UsersController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
 
         [HttpPost("authenticate")]
         public IActionResult Authenticate(AuthenticateRequest model)
         {
             var response = UsersMethods.Authenticate(model);
-
             if (response == null)
                 return BadRequest(new { message = "Username or password is incorrect" });          
 
@@ -32,6 +38,56 @@ namespace IM_Core.ApiControllers
         public IActionResult AllUsers()
         {
             return Ok(UsersMethods.GetAllUsers());
+        }
+
+        [HttpPost("AddUser")]
+        public async Task<IActionResult> AddUser()
+        {
+            User user = new User();
+            user.FirstName = HttpContext.Request.Form["FirstName"];
+            user.LastName = HttpContext.Request.Form["LastName"];
+            user.Email = HttpContext.Request.Form["Email"];
+            user.Phone = HttpContext.Request.Form["Phone"];
+            user.ProfilePic = HttpContext.Request.Form.Files.Count > 0 ? HttpContext.Request.Form.Files[0].FileName : "";
+
+            if (user == null || string.IsNullOrWhiteSpace(user.FirstName)
+                 || string.IsNullOrWhiteSpace(user.LastName) || string.IsNullOrWhiteSpace(user.Email)
+                )
+            {
+                return BadRequest(new { message = "Please enter all required fields." });                
+            }
+
+            var dbResponse = UsersMethods.AddUser(user);
+            if (dbResponse.Error)
+            {
+                if (dbResponse.ErrorMsg.Contains("UNQ__Users__Username"))
+                    return BadRequest(new { message = "Username already exists." });             
+                else if (dbResponse.ErrorMsg.Contains("UNQ__Users__Email"))
+                    return BadRequest(new { message = "Email already exists." });               
+                else
+                    return BadRequest(new { message = "Internal Error. " + dbResponse.ErrorMsg });               
+            }
+
+            string user_Id = dbResponse.Ds.Tables[0].Rows[0][0].ToString();
+
+            if (HttpContext.Request.Form.Files.Count > 0)
+            {
+                foreach (var formFile in HttpContext.Request.Form.Files)
+                {
+                    if (formFile.Length > 0)
+                    {
+                       Directory.CreateDirectory(_hostingEnvironment.ContentRootPath + "\\Attachments\\Users\\" + user_Id);
+                       string path = _hostingEnvironment.ContentRootPath + "\\Attachments\\Users\\" + user_Id + "\\" + formFile.FileName;
+                       
+                        using (var stream = new FileStream(path, FileMode.Create))
+                        {
+                           await formFile.CopyToAsync(stream);
+                        }
+                    }
+                }
+
+            }//end of if count > 0
+            return Ok("New User has been added.");
         }
 
         // GET: api/<UsersController>
