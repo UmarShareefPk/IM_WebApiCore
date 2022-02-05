4bb98a24f3fbf3c6e1501e95f4bd737bf8e31982
UmarShareefPk <76015950+UmarShareefPk@users.noreply.github.com>
2022-02-05 14:32:04 +0500

    Update DataAccessMethods.cs

--- SQL/DataAccessMethods.cs
+++ SQL/DataAccessMethods.cs
@@ -47,6 +47,7 @@ namespace IM.SQL
                 foreach (string k in parameters.Keys)
                     cmd.Parameters.AddWithValue(k, parameters[k]);
             }
+           // var reader = cmd.ExecuteReaderAsync();
             var da = new SqlDataAdapter(cmd);
             try
             {
@@ -64,6 +65,57 @@ namespace IM.SQL
             }
 
             return new DbResponse { Ds = ds, Error = false , ErrorMsg = null};
+        }
+
+        public  DbResponse ExecuteProcedureAsync2(string procedureName, SortedList<string, object> parameters)
+        {
+            var ds = new DataSet();
+            //var con = new SqlConnection(ConfigurationManager.ConnectionStrings["IMConString"].ConnectionString);
+            //var con = new SqlConnection("data source=localhost;initial catalog=IM;persist security info=True; Integrated Security=SSPI;");
+            
+          //  cmd.CommandTimeout = 0;
+                    
+
+            using (SqlConnection con2 = new SqlConnection(_connectionString.Main))
+            {
+                
+                using (SqlCommand cmd2 = new SqlCommand(procedureName, con2))
+                {
+                    cmd2.CommandType = CommandType.StoredProcedure;
+
+                    if (parameters != null)
+                    {
+                        foreach (string k in parameters.Keys)
+                            cmd2.Parameters.AddWithValue(k, parameters[k]);
+                    }
+
+
+                    con2.Open();
+                    var dr =   cmd2.ExecuteReader();
+                    dr.NextResult();
+                    while (dr.Read())
+                    {
+                        var s = dr["Title"].ToString();
+                    }
+                }
+            }       
+
+            try
+            {
+                // da.Fill(ds);
+               // await Task.Run(() => da.Fill(ds));
+
+            }
+            catch (Exception ex)
+            {
+                return new DbResponse { Ds = null, Error = true, ErrorMsg = ex.Message };
+            }
+            finally
+            {
+               // con2.Dispose();
+            }
+
+            return new DbResponse { Ds = ds, Error = false, ErrorMsg = null };
         }
     }// end of class
 }
